fe4c78e3209d658c34cf5d132ff58bdb36bf07ca
WizzOFF <maher_off@proton.me>
2025-03-18 21:45:20 +0100

    create_variables_using_dynamic_allocations.c: manually allocate memory to the system - Step 2: test the pointer to verify the value returned by malloc

--- create_variables_using_dynamic_allocation.c
+++ create_variables_using_dynamic_allocation.c
@@ -19,6 +19,18 @@ int main(int argc, char *argv[])
 
 void* malloc(size_t nombreOctetsNecessaires);
 
-int* memoireAllouee = NULL; // On crée un pointeur sur int
+int main(int argc, char *argv[])
+{
+    int* memoireAllouee = NULL;
 
-memoireAllouee = malloc(sizeof(int)); // La fonction malloc inscrit dans notre pointeur l'adresse qui a été reservée.
+    memoireAllouee = malloc(sizeof(int));
+    if (memoireAllouee == NULL) // On vérifie si la mémoire a été allouée
+    {
+        exit(0); // Erreur : on arrête tout !
+    }
+
+    // On peut utiliser ici la mémoire
+    free(memoireAllouee); // On n'a plus besoin de la mémoire, on la libère
+
+    return 0;
+}
